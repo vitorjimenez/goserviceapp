1e50d8ed8c4f0a0d4a8184b094bfe1f97fa20391
Your Name <you@example.com>
2023-08-21 17:07:19 -0300

    Adição das interfaces de serviços, e serviços usuário

--- src/main/java/com/soulcode/goserviceapp/controller/AuthController.java
+++ src/main/java/com/soulcode/goserviceapp/controller/AuthController.java
@@ -3,7 +3,7 @@ package com.soulcode.goserviceapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 @RequestMapping(value ="/auth")
@@ -11,6 +11,7 @@ public class AuthController {
 
     @GetMapping(value = "/login")
     public String login(){
+
         return "login";
     }
 
