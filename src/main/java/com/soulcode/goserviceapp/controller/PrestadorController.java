2d6a58f332f357ee71e2650492a6a419c8a687a9
Vitor <you@example.com>
2023-08-28 15:51:39 -0300

    Feat:Alterar dados do cliente

--- src/main/java/com/soulcode/goserviceapp/controller/PrestadorController.java
+++ src/main/java/com/soulcode/goserviceapp/controller/PrestadorController.java
@@ -4,6 +4,7 @@ import com.soulcode.goserviceapp.domain.Prestador;
 import com.soulcode.goserviceapp.domain.Servico;
 import com.soulcode.goserviceapp.service.PrestadorService;
 import com.soulcode.goserviceapp.service.ServicoService;
+import com.soulcode.goserviceapp.service.exceptions.ServicoNaoEncontradoException;
 import com.soulcode.goserviceapp.service.exceptions.UsuarioNaoAutenticadoException;
 import com.soulcode.goserviceapp.service.exceptions.UsuarioNaoEncontradoException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -35,6 +37,8 @@ public class PrestadorController {
             mv.addObject("prestador", prestador);
             List<Servico> especialidades = servicoService.findByPrestadorEmail(authentication.getName());
             mv.addObject("especialidades", especialidades);
+            List<Servico> servicos = servicoService.findAll();
+            mv.addObject("servicos", servicos);
         } catch (UsuarioNaoAutenticadoException | UsuarioNaoEncontradoException ex) {
             mv.addObject("errorMessage", ex.getMessage());
         } catch (Exception ex) {
@@ -44,12 +48,46 @@ public class PrestadorController {
     }
 
     @PostMapping(value = "/dados")
-    public String editarDados(Prestador prestador, RedirectAttributes attributes){
+    public String editarDados(Prestador prestador, RedirectAttributes attributes) {
         try {
             prestadorService.update(prestador);
-            attributes.addFlashAttribute("sucessMessage", "Dados alterados.");
-        }catch (Exception ex){
+            attributes.addFlashAttribute("successMessage", "Dados alterados.");
+        } catch (UsuarioNaoEncontradoException ex) {
+            attributes.addFlashAttribute("errorMessage", ex.getMessage());
+        } catch (Exception ex) {
             attributes.addFlashAttribute("errorMessage", "Erro ao alterar dados cadastrais.");
+        }
+        return "redirect:/prestador/dados";
+    }
+
+    @PostMapping(value = "/dados/especialidade/remover")
+    public String removerEspecialidade(
+            @RequestParam(name = "servicoId") Long id,
+            Authentication authentication,
+            RedirectAttributes attributes) {
+        try {
+            prestadorService.removeServicoPrestador(authentication, id);
+            attributes.addFlashAttribute("successMessage", "Especialidade removida");
+        } catch (UsuarioNaoAutenticadoException | UsuarioNaoEncontradoException | ServicoNaoEncontradoException ex) {
+            attributes.addFlashAttribute("errorMessage", ex.getMessage());
+        } catch (Exception ex) {
+            attributes.addFlashAttribute("errorMessage", "Erro ao remover especialidade.");
+        }
+        return "redirect:/prestador/dados";
+    }
+
+    @PostMapping(value = "/dados/especialidade/adicionar")
+    public String adicionarEspecialidade(
+            @RequestParam(name = "servicoId") Long id,
+            Authentication authentication,
+            RedirectAttributes attributes) {
+        try {
+            prestadorService.addServicoPrestador(authentication, id);
+            attributes.addFlashAttribute("successMessage", "Especialidade adicionada.");
+        } catch (UsuarioNaoAutenticadoException | UsuarioNaoEncontradoException | ServicoNaoEncontradoException ex) {
+            attributes.addFlashAttribute("errorMessage", ex.getMessage());
+        } catch (Exception ex) {
+            attributes.addFlashAttribute("errorMessage", "Erro ao adicionar nova especialidade.");
         }
         return "redirect:/prestador/dados";
     }
